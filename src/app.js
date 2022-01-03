17270b0b5d07df389f766f41ae0761afe10f2639
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-01-03 15:03:43 +0100

    Finished Project

--- src/app.js
+++ src/app.js
@@ -1,1 +1,168 @@
-console.log('Works');
+start();
+
+function start() {
+    const billInput = document.querySelector('.inputWrapper input');
+    const percentsWrapper = document.querySelector('.percentsWrapper');
+    const numPeopleInput = document.querySelector('.numberPeopleNormal');
+    const tipPerPersonP = document.querySelector('.tipPerPerson');
+    const totalTippP = document.querySelector('.tipTotal');
+    const customPercentInput = document.getElementById('customPercent');
+    const resetButton = document.querySelector('.resetButton');
+    const percentButtons = [...document.querySelectorAll('.percentBox')];
+    const warnMsgSpan = document.querySelector('.warnMsg');
+
+    let bill = 0;
+    let percents = 0;
+    let numPeople = 0;
+
+    let isEnabledResetButton = false;
+
+    billInput.addEventListener('change', (ev) => {
+        if(!isEnabledResetButton) {
+            enableResetButton();
+            isEnabledResetButton = true;
+        }
+        const input = Number(ev.target.value);
+
+        if(isNaN(bill)) {
+            return;
+        }
+
+        bill = input;
+
+        if(checkConditions()) {
+            print();
+        }
+    });
+
+    percentsWrapper.addEventListener('click', (ev) => {
+        if(ev.target.tagName == 'BUTTON') {
+            if(!isEnabledResetButton) {
+                enableResetButton();
+                isEnabledResetButton = true;
+            }
+            clearPercents();
+
+            percents = Number(ev.target.id); 
+
+            ev.target.classList.add('percentBoxActive');
+            
+            if(checkConditions()) {
+                print();
+            }
+        }
+    });
+
+    customPercentInput.addEventListener('focus', () => {
+        clearPercents();
+        customPercentInput.value = '';
+    });
+
+    customPercentInput.addEventListener('focusout', () => {
+        customPercentInput.value = percents == 0 ? 'Custom' : percents;
+    });
+
+    customPercentInput.addEventListener('change', (ev) => {
+        if(!isEnabledResetButton) {
+            enableResetButton();
+            isEnabledResetButton = true;
+        }
+
+        const input = Number(ev.target.value);
+
+        if(isNaN(input)) {
+            return;
+        }
+
+        percents = input;
+
+        if(checkConditions()) {
+            print();
+        }
+    });
+
+    numPeopleInput.addEventListener('change', (ev) => {
+
+        if(!isEnabledResetButton) {
+            enableResetButton();
+            isEnabledResetButton = true;
+        }
+
+        const input = Number(ev.target.value);
+
+        if(isNaN(input)) {
+            return;
+        }
+
+        if(input == 0) {
+            warnMsg(ev, true);
+            return;
+        }
+
+        warnMsg(ev, false);
+
+        numPeople = input;
+
+        if(checkConditions()) {
+            print();
+        }
+    });
+
+    resetButton.addEventListener('click', () => {
+        reset();
+    });
+
+    function checkConditions() {
+        if(bill != 0 && percents != 0 && numPeople != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function enableResetButton(){
+        resetButton.removeAttribute('disabled');
+    }
+
+    function totalAmount() {
+        return bill * percents / 100;
+    }
+
+    function tipPerPerson() {
+        return totalAmount() / numPeople;
+    }
+
+    function rprint() {
+        totalTippP.textContent = totalAmount().toFixed(2);
+        tipPerPersonP.textContent = tipPerPerson().toFixed(2);
+    }
+
+    function clearPercents() {
+        percentButtons.forEach(b => b.className = 'percentBox');
+        customPercentInput.value = 'Custom';
+        percents = 0;
+    }
+
+    function reset() {
+        clearPercents();
+        resetButton.setAttribute('disabled', 'true');
+        billInput.value = '';
+        numPeopleInput.value = '';
+        totalTippP.textContent = '$0.00';
+        tipPerPersonP.textContent = '$0.00';
+        isEnabledResetButton = false;
+    }
+
+    function warnMsg(ev, msgOn) {
+        if(msgOn) {
+            ev.target.classList.add('numberPeopleWarn');
+            warnMsgSpan.style.display = 'inline';
+            msgOn = true;
+        } else {
+            ev.target.classList.remove('numberPeopleWarn');
+            warnMsgSpan.style.display = 'none';
+            msgOn = false;
+        }
+    }
+
+};
